ced7132dc1aa94838538eb144fb6364fef7eafae
GitBrainsStudio <gitbrains@gmail.com>
2021-08-21 21:33:55 +0500

    fix main for running in gunicorn

--- Main.py
+++ Main.py
@@ -3,17 +3,10 @@ from Startup.DependenciesService import DependenciesService
 
 
 
+_dependenciesService:DependenciesService
+_dependenciesService = DependenciesService()
+_dependenciesService.RegisterControllers()
+_dependenciesService.GetUvicornService().Run()
 
-
-
-
-class Main() : 
-
-    _dependenciesService:DependenciesService
-
-    def __init__(self) -> None:
-        self._dependenciesService = DependenciesService()
-        self._dependenciesService.RegisterControllers()
-        self._dependenciesService.GetUvicornService().Run()
-      
-Main()
+def GetFastApi() : 
+    return _dependenciesService.GetFastApiService()._fastApi
